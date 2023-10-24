9eb6cc0bb2a30d0b1fbcc71db4b71853e065d546
Jacek <jacek_kozakowski_dev@wp.pl>
2023-10-24 06:50:07 +0200

    Version 1

---
+++ tic_tac_toe.js
@@ -0,0 +1,35 @@
+document.getElementById("square-1").addEventListener("click", (event) => {
+  console.log('1');
+});
+
+document.getElementById("square-2").addEventListener("click", (event) => {
+  console.log('2');
+});
+
+document.getElementById("square-3").addEventListener("click", (event) => {
+  console.log('3');
+});
+
+document.getElementById("square-4").addEventListener("click", (event) => {
+  console.log('4');
+});
+
+document.getElementById("square-5").addEventListener("click", (event) => {
+  console.log('5');
+});
+
+document.getElementById("square-6").addEventListener("click", (event) => {
+  console.log('6');
+});
+
+document.getElementById("square-7").addEventListener("click", (event) => {
+  console.log('7');
+});
+
+document.getElementById("square-8").addEventListener("click", (event) => {
+  console.log('8');
+});
+
+document.getElementById("square-9").addEventListener("click", (event) => {
+  console.log('9');
+});
